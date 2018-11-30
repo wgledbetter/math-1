93dc62dd7527eaa0f85230593120bdbed6265144
Daniel Lee <daniel@generable.com>
2018-11-30 00:08:16 -0500

    Revert "knockout more code"
    This reverts commit 54c705949378aef6e0e16906c6c64d81628a4b8b.

--- test/unit/math/rev/mat/fun/matrix_exp_multiply_test.cpp
+++ test/unit/math/rev/mat/fun/matrix_exp_multiply_test.cpp
@@ -84,7 +84,7 @@ inline void test_matrix_exp_multiply_vd(int N, int M) {
             << "): " << stan::math::ChainableStack::instance().var_stack_.size()
             << std::endl;
   std::cout << "Av = " << Av << std::endl;
-  /*// Eigen::Matrix<var, -1, -1> Bv = Eigen::Matrix<var, -1, -1>::Random(N, M);
+  // Eigen::Matrix<var, -1, -1> Bv = Eigen::Matrix<var, -1, -1>::Random(N, M);
   std::vector<stan::math::var> Avec = stan::math::to_array_1d(Av);
   std::cout << "stack size after Avec: "
             << stan::math::ChainableStack::instance().var_stack_.size()
@@ -115,7 +115,6 @@ inline void test_matrix_exp_multiply_vd(int N, int M) {
             << std::endl;
 
   std::cout << "res_vd = " << res_vd << std::endl;
-  */
   stan::math::print_stack(std::cout);
   /*
     EXPECT_FLOAT_EQ(res_vd.size(), expAB.size());
@@ -135,7 +134,7 @@ inline void test_matrix_exp_multiply_vd(int N, int M) {
   //     }
   //   }
   // }
-  /*res_vd(0, 0).grad(Avec, g);*/
+  res_vd(0, 0).grad(Avec, g);
   // expAB(0, 0).grad(Avec, g0);
   // for (size_t j = 0; j < g.size(); ++j) {
   // EXPECT_FLOAT_EQ(g[j], g0[j]);
