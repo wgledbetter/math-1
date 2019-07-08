d356de36ce33ea5efb34f52b1995727c227c03e9
Daniel Lee <daniel@generable.com>
2019-07-08 10:07:29 -0400

    flatten: prim/meta/append_return_type.hpp

--- stan/math/prim/meta.hpp
+++ stan/math/prim/meta.hpp
@@ -8,7 +8,8 @@
 
 #include <stan/math/prim/meta/length.hpp>
 
-#include <stan/math/prim/mat/meta/append_return_type.hpp>
+#include <stan/math/prim/meta/append_return_type.hpp>
+
 #include <stan/math/prim/mat/meta/as_array_or_scalar.hpp>
 #include <stan/math/prim/mat/meta/as_column_vector_or_scalar.hpp>
 #include <stan/math/prim/mat/meta/as_scalar.hpp>
